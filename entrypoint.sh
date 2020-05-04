cc57f946186c1c0604b9f806fd7defc81885b2cb
Alireza Bashiri <alible@fastmail.com>
2020-05-04 13:36:57 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,7 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-gem install -N standardrb $(version $INPUT_RUBOCOP_VERSION)
-
-# echo $INPUT_RUBOCOP_EXTENSIONS | xargs gem install -N
+gem install -N standard $(version $INPUT_RUBOCOP_VERSION)
 
 standardrb ${INPUT_RUBOCOP_FLAGS} \
-  | reviewdog -f=rubocop -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -f=rubocop -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
